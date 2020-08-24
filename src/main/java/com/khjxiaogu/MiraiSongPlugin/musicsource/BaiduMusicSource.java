cb0a05d3e50e0313271988fdbc555eee5019dc59
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2020-08-25 02:36:54 +0800

    fixed unstable

--- src/main/java/com/khjxiaogu/MiraiSongPlugin/musicsource/BaiduMusicSource.java
+++ src/main/java/com/khjxiaogu/MiraiSongPlugin/musicsource/BaiduMusicSource.java
@@ -16,16 +16,27 @@ public class BaiduMusicSource implements MusicSource {
 
 	@Override
 	public MusicInfo get(String keyword) throws Exception {
+		
 		HttpURLConnection huc=(HttpURLConnection) new URL("http://tingapi.ting.baidu.com/v1/restserver/ting?format=json&calback=&from=webapp_music&method=baidu.ting.search.catalogSug&query="+keyword).openConnection();
+		huc.setRequestProperty("Host","tingapi.ting.baidu.com");
 		huc.setRequestMethod("GET");
 		huc.connect();
-		String sid=JsonParser.parseString(new String(Utils.readAll(huc.getInputStream()), "UTF-8"))
+		JsonObject jo=JsonParser.parseString(new String(Utils.readAll(huc.getInputStream()), "UTF-8")).getAsJsonObject();
+		String sid=jo.getAsJsonObject()
 		.getAsJsonObject().get("song").getAsJsonArray().get(0).getAsJsonObject().get("songid")
 		.getAsString();
 		huc=(HttpURLConnection) new URL("http://tingapi.ting.baidu.com/v1/restserver/ting?format=json&calback=&from=webapp_music&method=baidu.ting.song.playAAC&songid="+sid).openConnection();
+		huc.setRequestProperty("Host","tingapi.ting.baidu.com");
+		huc.setRequestMethod("GET");
+		huc.connect();
 		JsonObject allinfo=JsonParser.parseString(new String(Utils.readAll(huc.getInputStream()), "UTF-8")).getAsJsonObject();
 		JsonObject sif=allinfo.getAsJsonObject().get("songinfo").getAsJsonObject();
-		return new MusicInfo(sif.get("title").getAsString(),sif.get("author").getAsString(),allinfo.get("pic_big").getAsString(),allinfo.get("bitrate").getAsJsonObject().get("file_link").getAsString(),sif.get("share_url").getAsString(),"千千静听","http://pic.17qq.com/uploads/epihigfbdz.jpeg");
+		return new MusicInfo(sif.get("title").getAsString(),
+				sif.get("author").getAsString(),
+				sif.get("pic_big").getAsString(),
+				allinfo.get("bitrate").getAsJsonObject().get("file_link").getAsString(),
+				sif.get("share_url").getAsString(),
+				"千千静听");
 	}
 
 }
