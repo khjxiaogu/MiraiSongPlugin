25b37e5219a64ee518971f4d3a62a781ba388b36
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2020-08-31 21:51:44 +0800

    javadoc renew

--- src/main/java/com/khjxiaogu/MiraiSongPlugin/MusicCardProvider.java
+++ src/main/java/com/khjxiaogu/MiraiSongPlugin/MusicCardProvider.java
@@ -1,11 +1,7 @@
 package com.khjxiaogu.MiraiSongPlugin;
 
-import java.net.MalformedURLException;
-
 import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.message.data.Message;
-// TODO: Auto-generated Javadoc
-import net.mamoe.mirai.utils.OverFileSizeMaxException;
 
 /**
  * Interface MusicCardProvider.
@@ -23,8 +19,7 @@ public interface MusicCardProvider {
 	 * @param mi 音乐信息
 	 * @param ct 请求该信息的联系人，可能为群或者好友，用于上传图片等用，不要直接给他们发信息，应该通过返回值
 	 * @return 返回要回传的音乐卡片信息
-	 * @throws MalformedURLException 
-	 * @throws OverFileSizeMaxException 
+	 * @throws Exception 发生任意异常 
 	 */
 	public Message process(MusicInfo mi,Contact ct) throws Exception;
 }
