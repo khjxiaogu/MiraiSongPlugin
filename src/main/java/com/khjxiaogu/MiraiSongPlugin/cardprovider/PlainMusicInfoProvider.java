1e1d3a4b1597b96e4d03169eacd200ab662f886b
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2020-09-11 00:42:42 +0800

    minor fixes

--- src/main/java/com/khjxiaogu/MiraiSongPlugin/cardprovider/PlainMusicInfoProvider.java
+++ src/main/java/com/khjxiaogu/MiraiSongPlugin/cardprovider/PlainMusicInfoProvider.java
@@ -20,14 +20,12 @@ public class PlainMusicInfoProvider implements MusicCardProvider {
 	public Message process(MusicInfo mi, Contact ct) throws OverFileSizeMaxException, MalformedURLException {
 		Image im=ct.uploadImage(new URL(mi.purl));
 		StringBuilder tsb=new StringBuilder()
-				.append("歌名：")
-				.append(mi.title)
-				.append("\n作者：")
-				.append(mi.desc)
-				.append("\n封面："+mi.purl)
-				.append("\n外链："+mi.murl)
-				.append("\n链接："+mi.jurl)
-				.append("\n来自："+mi.source);
+				.append("歌名：").append(mi.title)
+				.append("\n作者：").append(mi.desc)
+				.append("\n封面：").append(mi.purl)
+				.append("\n外链：").append(mi.murl)
+				.append("\n链接：").append(mi.jurl)
+				.append("\n来自：").append(mi.source);
 		return im.plus(tsb.toString());
 	}
 
