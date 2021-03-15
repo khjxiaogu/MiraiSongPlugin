851de90a67348dda4fefb2889fff5b2fcf8971a2
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-03-16 03:19:01 +0800

    added new sources

--- src/test/java/com/khjxiaogu/MiraiSongPlugin/test/Main.java
+++ src/test/java/com/khjxiaogu/MiraiSongPlugin/test/Main.java
@@ -9,7 +9,9 @@ import com.khjxiaogu.MiraiSongPlugin.MusicInfo;
 import com.khjxiaogu.MiraiSongPlugin.MusicSource;
 import com.khjxiaogu.MiraiSongPlugin.musicsource.BaiduMusicSource;
 import com.khjxiaogu.MiraiSongPlugin.musicsource.KugouMusicSource;
+import com.khjxiaogu.MiraiSongPlugin.musicsource.NetEaseAdvancedRadio;
 import com.khjxiaogu.MiraiSongPlugin.musicsource.NetEaseMusicSource;
+import com.khjxiaogu.MiraiSongPlugin.musicsource.NetEaseRadioSource;
 import com.khjxiaogu.MiraiSongPlugin.musicsource.QQMusicSource;
 
 public class Main {
@@ -19,6 +21,7 @@ public class Main {
 		sources.put("QQ音乐", new QQMusicSource());
 		// sources.put("QQ音乐HQ",new QQMusicHQSource());//这个音乐源已被tx禁用。
 		sources.put("网易", new NetEaseMusicSource());
+		sources.put("网易电台", new NetEaseRadioSource());
 		sources.put("酷狗", new KugouMusicSource());
 		sources.put("千千", new BaiduMusicSource());
 	}
@@ -27,7 +30,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) throws Exception {
-		MusicInfo mi = sources.get("酷狗").get(URLEncoder.encode("Rain on me"));
+		MusicInfo mi = sources.get("网易电台").get(URLEncoder.encode("屑"));
 		StringBuilder tsb = new StringBuilder().append("歌名：").append(mi.title).append("\n作者：").append(mi.desc)
 				.append("\n封面：" + mi.purl).append("\n外链：" + mi.murl).append("\n链接：" + mi.jurl)
 				.append("\n来自：" + mi.source);
