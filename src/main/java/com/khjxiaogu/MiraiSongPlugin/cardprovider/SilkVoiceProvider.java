dab029b035238fd971ceca415ff8b8c7bdc3c5a7
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-08-21 15:10:26 +0800

    removed silk4j

--- src/main/java/com/khjxiaogu/MiraiSongPlugin/cardprovider/SilkVoiceProvider.java
+++ src/main/java/com/khjxiaogu/MiraiSongPlugin/cardprovider/SilkVoiceProvider.java
@@ -2,7 +2,9 @@ package com.khjxiaogu.MiraiSongPlugin.cardprovider;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
@@ -12,23 +14,14 @@ import com.khjxiaogu.MiraiSongPlugin.MusicInfo;
 import com.khjxiaogu.MiraiSongPlugin.Utils;
 
 import net.mamoe.mirai.contact.Contact;
-import net.mamoe.mirai.contact.Group;
 import net.mamoe.mirai.message.data.Message;
 import net.mamoe.mirai.message.data.PlainText;
 import net.mamoe.mirai.utils.ExternalResource;
 
-import io.github.mzdluo123.silk4j.AudioUtils;
 public class SilkVoiceProvider implements MusicCardProvider {
 	public static String silk = "silk_v3_encoder";
 	public static String ffmpeg ="ffmpeg";
-	static {
-		try {
-			AudioUtils.init(new File("temp/"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+
 	public SilkVoiceProvider() {
 	}
 
@@ -45,22 +38,23 @@ public class SilkVoiceProvider implements MusicCardProvider {
 		} catch (IOException e) {
 			return new PlainText("获取音频失败");
 		}
-		//File f = new File("temp/", "wv" + System.currentTimeMillis() + ".m4a");
-		//File ft = new File("temp/", "wv" + System.currentTimeMillis() + ".mp3");
+		File f = new File("temp/", "wv" + System.currentTimeMillis() + ".m4a");
+		File f2 = new File("temp/", "wv" + System.currentTimeMillis() + ".silk");
+		File ft = new File("temp/", "wv" + System.currentTimeMillis() + ".pcm");
 		// File f2=new File("./temp/","wv"+System.currentTimeMillis()+".amr");
-		File f2=null;
 		try {
-			//f.getParentFile().mkdirs();
-			//OutputStream os = new FileOutputStream(f);
-			//os.write(Utils.readAll(huc2.getInputStream()));
+			f.getParentFile().mkdirs();
+			OutputStream os = new FileOutputStream(f);
+			os.write(Utils.readAll(huc2.getInputStream()));
 
-			//os.close();
+			os.close();
 			// exeCmd(new File("ffmpeg.exe").getAbsolutePath() + " -i \"" +
 			// f.getAbsolutePath()
 			// + "\" -ab 12.2k -ar 8000 -ac 1 -y " + f2.getAbsolutePath());
-			//Utils.exeCmd(ffmpeg, "-i", f.getAbsolutePath(), "-y", ft.getAbsolutePath());
-			f2=AudioUtils.mp3ToSilk(huc2.getInputStream());
-			
+			Utils.exeCmd(ffmpeg, "-i", f.getAbsolutePath(), "-f", "s16le", "-ar", "24000", "-ac", "1",
+					"-acodec", "pcm_s16le", "-y", ft.getAbsolutePath());
+			Utils.exeCmd(silk, ft.getAbsolutePath(), f2.getAbsolutePath(), "-Fs_API", "24000",
+					"-tencent");
 			try (FileInputStream fis = new FileInputStream(f2);ExternalResource ex=ExternalResource.create(fis)) {
 				return Utils.uploadVoice(ex,ct);
 			}
@@ -68,10 +62,9 @@ public class SilkVoiceProvider implements MusicCardProvider {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} finally {
-			//f.delete();
-			//ft.delete();
-			if(f2!=null)
-				f2.delete();
+			f.delete();
+			ft.delete();
+			f2.delete();
 		}
 		return new PlainText("当前状态不支持音频");
 	}
